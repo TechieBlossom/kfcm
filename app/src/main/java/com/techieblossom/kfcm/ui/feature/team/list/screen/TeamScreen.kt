7d0239b40128e27c96bc215e478edf13dd93e942
Prateek Sharma/Users/prateeksharma/techieblossom/kfcm/.git/config <programmersine@gmail.com>
2023-11-17 16:39:15 +0530

    Temp changes

--- app/src/main/java/com/techieblossom/kfcm/ui/feature/team/list/screen/TeamScreen.kt
+++ app/src/main/java/com/techieblossom/kfcm/ui/feature/team/list/screen/TeamScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,9 +42,6 @@ fun TeamScreen(onSearchTapped: () -> Unit, onTeamSelected: (teamId: Int) -> Unit
             viewModel.canPaginate && (lazyStaggeredGridState.layoutInfo.visibleItemsInfo.lastOrNull()?.index
                 ?: -9) >= (lazyStaggeredGridState.layoutInfo.totalItemsCount - 4)
         }
-    }
-    val teamGridVisibleState by remember {
-        mutableStateOf(derivedStateOf { teamUiState.teams.isNotEmpty() })
     }
 
     LaunchedEffect(key1 = shouldStartPaginate.value) {
